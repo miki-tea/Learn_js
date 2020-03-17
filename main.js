b2f07773e37a00d86264e6ecaf72851bfb50f019
miki <miki.ishii16@gmail.com>
2020-03-18 01:26:19 +0900

    confirmメソッド

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-(function() {
-  "use strict";
+var btn = document.getElementById("btn");
+var msgBox = document.getElementById("msgBox");
 
-})();
+btn.addEventListener("click", function() {
+  var result = window.confirm("どちらかを選んでね！");
+  if (result) {
+    console.log("OK");
+    msgBox.innerText = "OK";
+  } else {
+    console.log("キャンセル");
+    msgBox.innerText = "キャンセル";
+  }
+});
