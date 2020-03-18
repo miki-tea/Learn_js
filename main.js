93c8687b7361d41592e985fd61bb012b2bd8d7c4
miki <miki.ishii16@gmail.com>
2020-03-18 14:34:15 +0900

    テキストからキーワードを検索する機能追加

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var btn = document.getElementById("btn");
-var msgBox = document.getElementById("msgBox");
+let btn = document.getElementById("btn");
+let msgBox = document.getElementById("msgBox");
 
-btn.addEventListener("click", function() {
-  var result = window.confirm("どちらかを選んでね！");
+btn.addEventListener("click", () => {
+  let result = window.confirm("どちらかを選んでね！");
   if (result) {
     console.log("OK");
     msgBox.innerText = "OK";
@@ -10,4 +10,31 @@ btn.addEventListener("click", function() {
     console.log("キャンセル");
     msgBox.innerText = "キャンセル";
   }
+});
+
+let repBtn = document.getElementById("rep-btn");
+
+repBtn.addEventListener("click", () => {
+  console.log("repBtnがクリックされた");
+  let refTxtVal = document.getElementById("reference-txt").value;
+  let orgKey = document.getElementById("original-key").value;
+  let orgKeyMatch = new RegExp(
+    document.getElementById("original-key").value,
+    "g"
+  );
+  let repKey = document.getElementById("replace-key").value;
+  let resArea = document.getElementById("result");
+  //キーワードマッチ数
+
+  let count = (refTxtVal.match(orgKeyMatch) || []).length;
+  //未入力チェック
+  if (orgKey == "" || repKey == "" || refTxtVal == "") {
+    window.alert("未入力欄があります");
+    return;
+  }
+  // 結果表示
+  resArea.innerText = `ワードヒット数:${count}件\n${refTxtVal.replace(
+    orgKeyMatch,
+    repKey
+  )}`;
 });
